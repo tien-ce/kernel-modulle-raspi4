e223152fb1cb4fbfb718e31cf8a8b20e714b386d
vantien <tien.nguyen11102004@hcmut.edu.vn>
2025-06-22 04:00:07 +0100

    hello_world_kernel

---
+++ hello_world.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Van Tien");
+MODULE_DESCRIPTION("example Linux module");
+MODULE_VERSION("0.01");
+int a1;
+EXPORT_SYMBOL(a1);
+int init_module(void)
+{
+	printk(KERN_INFO "Hello world 1.\n");
+	a1 = 10;
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
+
