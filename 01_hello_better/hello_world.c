af1893272abaa6eef9240d9471d05faaa197e9d5
tien-ce <tien11102004@hcmut.edu.vn>
2025-12-31 14:20:46 +0700

    current process

--- 01_hello_better/hello_world.c
+++ 01_hello_better/hello_world.c
@@ -1,22 +1,18 @@
-// SPDX-License-Identifier: GPL-2.0
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/module.h>
+#include <asm/current.h>
+MODULE_LICENSE ("Dual BSD/GPL");
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Van Tien");
-MODULE_DESCRIPTION("example Linux module");
-MODULE_VERSION("0.01");
-// extern int a1;
-static int __init my_init_module(void)
-{
-	pr_info("Hello world 2.\n");
+static __init int hello_init(void){
+	pr_info ("Hello, world\n");
+	pr_info ("The current process is %s pid %d\n",current->comm, current->pid);
+		
 	return 0;
 }
 
-static void __exit my_cleanup_module(void)
-{
-	pr_info("Goodbye world 2.\n");
+static __exit void hello_exit(void){
+	pr_info ("Goodbye, cruel world\n");
 }
-module_init(my_init_module);
-module_exit(my_cleanup_module);
+
+module_init (hello_init);
+module_exit (hello_exit);
